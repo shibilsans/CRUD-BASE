20c6a7d4d0dad3dc988cc255bbe55fe17fb67ee9
shibilsan <muahmmedshibily33@gmail.com>
2024-02-12 21:13:07 +0530

    first title change

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">';
-    <title>Document</title>
+    <title>INDEX</title>
 </head>
 <body>
     <form action="create.php" method="post">
